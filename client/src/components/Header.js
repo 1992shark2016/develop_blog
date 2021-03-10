16f2f5b1e3f31b8819b05516b37857fca7d17c49
Heeseob Kim <skeptic600@gmail.com>
2021-03-10 23:09:01 +0900

    Footer Header setting

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div id="page-header" className="mb-3">
+            <Row>
+                <Col md="6" sm="auto" className="text-center m-auto">
+                    <h1>Read Our Blog</h1>
+                    <p>사이드 프로젝트 작업 블로그일걸요?</p>
+                </Col>
+            </Row>
+        </div>
+    )
 };
 
 export default Header;
